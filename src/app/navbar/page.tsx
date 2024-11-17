ad3f25c13d5dc125ff91d52197f3171fc6e82ff7
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-11-17 19:49:38 +0100

    navbar created

---
+++ src/app/navbar/page.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-lime-100 p-4">
+      navbar
+      <ul className="flex flex-row gap-3">
+        <div>eins</div>
+        <div>zwei</div>
+        <div>drei</div>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
