cd8b36e03852c4b20bcfd0396054383480d932b0
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-07-15 12:59:40 +0200

    delete unused code

--- web-frontend/src/pages/main/page.tsx
+++ web-frontend/src/pages/main/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useRouter } from 'next/navigation';
+import { Button } from "../../components/ui/button";
 
 const Main = () => {
 
-  const router = useRouter();
-
-  const toDashboard = () => {
-    router.push("/main/dashboard");
-  }
   return (
     <main className="flex-grow p-4 mb-20">
 
-      <Button onClick={toDashboard}>
+      <Button>
         zum Dashboard
       </Button>
 
