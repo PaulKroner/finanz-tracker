d0df7982de5306c5bc15b4557d9d25de92d65986
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-07-23 18:16:51 +0200

    add navigation

--- web-frontend/src/components/navbar/page.tsx
+++ web-frontend/src/components/navbar/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from '../../components/ui/button';
 import { FaChartLine } from "react-icons/fa6";
 import { IoPersonCircle } from "react-icons/io5";
+import { IoStatsChart } from "react-icons/io5";
 import AddIncomeExpenseButton from './addIncomeExpenseButton';
+import { Link } from 'react-router';
 
 
 const Navbar = () => {
@@ -9,18 +11,28 @@ const Navbar = () => {
   return (
     <nav className="bg-lime-100 p-2">
       <ul className="flex flex-row flex-wrap gap-3 justify-around">
-        {/* <Button onClick={() => setView("income")}>Einnahme hinzufügen</Button>
-        <Button onClick={() => setView("expanse")}>Ausgabe hinzufügen</Button>
-        <Button onClick={() => setView("dashboard")}>zum Dashboard</Button> */}
-        <Button className="flex flex-col h-12 p-4">
-          <div className="">
-            <FaChartLine size={10} />
-          </div>
-          <div className="text-xs">
-            Dashboard
-          </div>
-        </Button>
 
+        <Link to="/dashboard">
+          <Button className="flex flex-col h-12 p-4">
+            <div className="">
+              <FaChartLine size={10} />
+            </div>
+            <div className="text-xs">
+              Dashboard
+            </div>
+          </Button>
+        </Link>
+
+        <Link to="/details">
+          <Button className="flex flex-col h-12 p-4">
+            <div className="">
+              <IoStatsChart size={10} />
+            </div>
+            <div className="text-xs">
+              Details
+            </div>
+          </Button>
+        </Link>
 
         {/* Income / Expense Insert */}
         <AddIncomeExpenseButton />
