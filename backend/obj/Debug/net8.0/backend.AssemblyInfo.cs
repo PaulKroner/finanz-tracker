deeb0d7673a1df53756b2e3f79c5502cc831fae8
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-07-24 14:20:50 +0200

    backend migration update

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0f33116f07e9caf4ab9e25d8fee27d29a47bf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a99cd63f150ee6624c7c9b601feda977707732")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
