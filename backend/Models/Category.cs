127212a2f830bd0c7154226842d5ab26d722292f
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-08-04 15:09:08 +0200

    add auto increment to id

--- backend/Models/Category.cs
+++ backend/Models/Category.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.Models
 {
   public class Category
   {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     // navigation property for 1:n
