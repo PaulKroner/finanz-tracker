907f556b52708defbbbdf21d1c4f6d40c097605d
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-09-04 12:34:30 +0200

    react native setup

---
+++ mobile-frontend/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+
+import { Text, View } from '@/components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-bold">Tab One</Text>
+      <View />
+    </View>
+  );
+}
